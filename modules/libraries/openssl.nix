b57857978b7fedc0862c25d0151595f5f3c47d1d
cubewhy <qby140326@gmail.com>
2026-02-13 11:39:02 +0800

    fix: let nix to management pkg-config for openssl

--- modules/libraries/openssl.nix
+++ modules/libraries/openssl.nix
@@ -8,21 +8,20 @@ with lib; let
   cfg = config.blackbox.libraries.openssl;
 in {
   options.blackbox.libraries.openssl = {
-    enable = mkEnableOption "OpenSSL development library (includes pkg-config setup)";
+    enable = mkEnableOption "OpenSSL library";
   };
 
   config = mkIf cfg.enable {
     blackbox.packages = [
-      pkgs.openssl
       pkgs.pkg-config
+      pkgs.openssl
+      pkgs.openssl.dev
     ];
 
     blackbox.env = {
       OPENSSL_DIR = "${pkgs.openssl.dev}";
       OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
       OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
-
-      PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
 
       OPENSSL_NO_VENDOR = "1";
     };
