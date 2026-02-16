f0ed4afc0f3fcef32936f60f78d989e27274d3a0
cubewhy <qby140326@gmail.com>
2026-02-16 22:20:57 +0800

    Revert "fix(rust): apply mcfgthread workaround"
    This reverts commit a7ec6879f01bf8adae7cb41424204a1f5fa953fa.

--- modules/languages/rust.nix
+++ modules/languages/rust.nix
@@ -118,14 +118,9 @@ in {
             -L native=${mingwPthreads}/lib
           '';
 
-          CARGO_TARGET_I686_PC_WINDOWS_GNU_RUSTFLAGS = [
-            "-L"
-            "${mingw32Pkgs.stdenv.cc.cc}/lib/gcc/i686-w64-mingw32/${mingw32Pkgs.stdenv.cc.version}"
-            "-L"
-            "${mingw32Pkgs.windows.mcfgthread}/lib"
-            "-l"
-            "mcfgthread"
-          ];
+          CARGO_TARGET_I686_PC_WINDOWS_GNU_RUSTFLAGS = ''
+            -L native=${mingwPthreads}/lib
+          '';
         }
       ];
     })
