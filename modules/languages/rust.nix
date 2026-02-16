7ffa921aef65b401d2305b19f7c3922c7650d8ef
cubewhy <qby140326@gmail.com>
2026-02-16 22:20:55 +0800

    Revert "fix(rust): use i686 pthread lib"
    This reverts commit 1acbfa18d8b1cccbd1c7460a7a6b93d5b99f53c7.

--- modules/languages/rust.nix
+++ modules/languages/rust.nix
@@ -122,7 +122,9 @@ in {
             "-L"
             "${mingw32Pkgs.stdenv.cc.cc}/lib/gcc/i686-w64-mingw32/${mingw32Pkgs.stdenv.cc.version}"
             "-L"
-            "${mingw32Pkgs.windows.pthreads}/lib"
+            "${mingw32Pkgs.windows.mcfgthread}/lib"
+            "-l"
+            "mcfgthread"
           ];
         }
       ];
